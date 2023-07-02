ca214803c92f3683ff094358421ea20c9d374d6d
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-07-02 23:13:07 +0200

    Add initial routing logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import './assets/App.css';
+import Initial from './components/Initial';
+import Confirm from './components/Confirm';
+import Result from './components/Result';
+import VideoBackground from './components/VideoBackground';
 
 const App = () => {
+
+  const handleComponent = () => {
+    const screen = 0;
+    if (screen === 0) return <Initial />
+    if (screen === 1) return <Confirm />
+    if (screen === 2) return <Result />
+  }
+
   return (
     <div>
-      Hello
+      <VideoBackground />
+      <div className='container'>
+        {handleComponent()}
+      </div>
     </div>
   );
 }
