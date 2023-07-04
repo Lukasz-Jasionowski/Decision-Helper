cf5c15e9f957dd6f7eb67ed5a3304b8952031fc0
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-07-04 19:01:36 +0200

    Add Context and use its state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useContext } from 'react';
+import { MyContext } from './context';
 import './assets/App.css';
 import Initial from './components/Initial';
 import Confirm from './components/Confirm';
@@ -7,8 +9,10 @@ import VideoBackground from './components/VideoBackground';
 
 const App = () => {
 
+  const context = useContext(MyContext);
+
   const handleComponent = () => {
-    const screen = 0;
+    const screen = context.state.screen;
     if (screen === 0) return <Initial />
     if (screen === 1) return <Confirm />
     if (screen === 2) return <Result />
