13570c335577a029172d1eafb133c698420c9d06
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-07-05 21:10:58 +0200

    Add answer array, result state, getRandomValue, handleResult, and handleReset

--- src/context/index.js
+++ src/context/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
+const answer = [
+    'Yes', 'No', 'Maybe', 'You should know', 'Ask a friends', 'You know the best'
+]
+
 const MyContext = React.createContext();
 class MyProvider extends Component {
 
     state = {
         screen: 0,
-        question: ''
+        question: '',
+        result: ''
     }
 
     handleGoTo = (value) => {
@@ -16,13 +21,37 @@ class MyProvider extends Component {
         this.setState({ question: value })
     }
 
+    getRandomValue = () => {
+        return answer[Math.floor(Math.random() * answer.length)]
+    }
+
+    handleResult = () => {
+        let rand = this.getRandomValue();
+        if (this.state.result !== '') {
+            while (rand === this.state.result) {
+                rand = this.getRandomValue();
+            }
+        }
+        this.setState({ result: rand })
+    }
+
+    handleReset = () => {
+        this.setState({
+            screen: 0,
+            question: '',
+            result: ''
+        })
+    }
+
     render() {
         return (
             <>
                 <MyContext.Provider value={{
                     state: this.state,
                     goTo: this.handleGoTo,
-                    question: this.handleQuestion
+                    question: this.handleQuestion,
+                    result: this.handleResult,
+                    reset: this.handleReset
                 }}
                 >
                     {this.props.children}
